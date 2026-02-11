27ac0eb9d22f54414ba213c036bdb1369f4c7549
mnfasihi <fasihi.mn@gmail.com>
2026-02-11 22:11:25 +0330

    اضافه کردن Supabase Client

---
+++ supabaseClient.js
@@ -0,0 +1,8 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+const supabaseUrl = 'https://vohwipmyblmvyxiguknv.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvaHdpcG15Ymxtdnl4aWd1a252Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1Mjg3MzMsImV4cCI6MjA4NjEwNDczM30.UvOVxq0hX3AWymr_nhTLThH_oAVn3KG05SafOprfs6Q'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
+window.supabase = supabase
+console.log('✅ Supabase connected')
